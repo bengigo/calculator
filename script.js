eb640903fc3e8b6a9bec1a22b50d5aeb802ec64e
bengigenc <genc.bengi@gmail.com>
2022-06-22 19:09:27 +0300

    Create a function to call simple math functions according to operators

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ function divide(previusOperand, currentOperand) {
   return previusOperand / currentOperand;
 }
 
+// convert functions above to class methods
+
+function operate(operator, previusOperand, currentOperand) {
+//   switch (operator) {
+//     case '+':
+//       add(previusOperand, currentOperand);
+//       break;
+//     case '-':
+//       substract(previusOperand, currentOperand);
+//       break;
+//     case '*':
+//       multiply(previusOperand, currentOperand);
+//       break;
+//     case '÷':
+//       divide(previusOperand, currentOperand);
+//       break;
+//     default:
+//   }
+  if (operator === '+') {
+     return add(previusOperand, currentOperand);
+  } else if (operator === '-') {
+    return substract(previusOperand, currentOperand);
+  } else if (operator === '*') {
+    return multiply(previusOperand, currentOperand);
+  } else if (operator === '÷') {
+    return divide(previusOperand, currentOperand);
+  }
+}
