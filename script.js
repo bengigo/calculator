b697284e3ddb9accc7b141548f0ec25995a999f5
bengigenc <genc.bengi@gmail.com>
2022-06-22 18:58:58 +0300

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(previusOperand, currentOperand) {
+  return previusOperand + currentOperand;
+}
+
+function substract(previusOperand, currentOperand) {
+  return previusOperand - currentOperand;
+}
+
+function multiply(previusOperand, currentOperand) {
+  return previusOperand * currentOperand;
+}
+
+function divide(previusOperand, currentOperand) {
+  return previusOperand / currentOperand;
+}
+
